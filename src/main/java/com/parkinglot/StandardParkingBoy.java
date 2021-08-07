110eed609b0343c7dfd9b8dad19e7299bdf09b4b
omagara <omagara@oocl.com>
2021-08-08 07:56:36 +0800

    [ParkingLot-Story4][Ramon]feat: implement should_return_the_right_car_with_each_ticket_when_fetch_car_twice_given_two_parking_lots_with_two_parked_cars_a_standard_parking_boy_and_two_parked_tickets

--- src/main/java/com/parkinglot/StandardParkingBoy.java
+++ src/main/java/com/parkinglot/StandardParkingBoy.java
@@ -19,13 +19,25 @@ public class StandardParkingBoy {
     }
 
     public ParkingTicket park(Car car) {
-        if(parkingLots!=null && !parkingLots.get(0).isFullyOccupied()){
-            return  parkingLots.get(0).park(car);
+        if(parkingLots!=null){
+            if (!parkingLots.get(0).isFullyOccupied()) {
+                return parkingLots.get(0).park(car);
+            }
+            if (parkingLots.get(0).isFullyOccupied()){
+                return parkingLots.get(1).park(car);
+            }
         }
-        return parkingLots.get(1).park(car);
+        return parkingLot.park(car);
     }
 
     public Car fetch(ParkingTicket parkingTicket) {
+        if(parkingLots!=null) {
+            for (ParkingLot parkingLot : parkingLots) {
+                if (parkingLot.getParkedPosition().containsKey(parkingTicket)) {
+                    return parkingLot.fetch(parkingTicket);
+                }
+            }
+        }
         return parkingLot.fetch(parkingTicket);
     }
 
